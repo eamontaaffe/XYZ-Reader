23b3674c0d750c48d39543e156440ed674191bae
eamon <eamontaaffe@gmail.com>
2015-09-12 19:16:06 +1000

    aspect ratio is now being enforced

--- XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
+++ XYZReader/src/main/java/com/example/xyzreader/ui/DynamicHeightNetworkImageView.java
@@ -27,8 +27,11 @@ public class DynamicHeightNetworkImageView extends NetworkImageView {
 
     @Override
     protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
-        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
         int measuredWidth = getMeasuredWidth();
-        setMeasuredDimension(measuredWidth, (int) (measuredWidth / mAspectRatio));
+        int measuredHeight = (int) (measuredWidth *2/3);
+
+        int dymanicHeightMeasureSpec = MeasureSpec.makeMeasureSpec(measuredHeight, MeasureSpec.EXACTLY);
+
+        super.onMeasure(widthMeasureSpec,dymanicHeightMeasureSpec);
     }
 }
